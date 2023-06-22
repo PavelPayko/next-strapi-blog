ef9abe3540924228b78ea6a7a257d27b90acabe6
Pavel Payko <mail@pavelpayko.ru>
2023-06-22 09:52:54 +0000

    config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["localhost", "127.0.0.1"],
+    domains: ["localhost", "127.0.0.1", "45.84.225.246"],
   },
   i18n: {
     locales: ['en', 'ru'],
